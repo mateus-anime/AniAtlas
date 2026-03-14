4af581096a1d032d3060a1a6df155c320ff1d379
mateus-anime <tewshenr@gmail.com>
2026-03-14 11:49:20 -0300

    Update mangadex.js

--- api/mangadex.js
+++ api/mangadex.js
@@ -1,18 +1,29 @@
-export default async function handler(request, response) {
-  const query = request.url.split("?")[1] || "";
-  const url = `https://api.mangadex.org/manga?${query}`;
+module.exports = async (req, res) => {
+  const queryString = req.url.includes("?")
+    ? req.url.slice(req.url.indexOf("?"))
+    : "";
+
+  const targetUrl = `https://api.mangadex.org/manga${queryString}`;
 
   try {
-    const api = await fetch(url);
-    const data = await api.json();
+    const apiResponse = await fetch(targetUrl, {
+      headers: {
+        Accept: "application/json"
+      }
+    });
 
-    response.setHeader("Access-Control-Allow-Origin", "*");
-    response.status(200).json(data);
+    const text = await apiResponse.text();
 
-  } catch (err) {
-    response.status(500).json({
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "*");
+    res.setHeader("Content-Type", "application/json; charset=utf-8");
+
+    return res.status(apiResponse.status).send(text);
+  } catch (error) {
+    return res.status(500).json({
       error: "proxy_error",
-      message: err.message
+      message: error.message || "Erro ao buscar MangaDex"
     });
   }
-}
+};
